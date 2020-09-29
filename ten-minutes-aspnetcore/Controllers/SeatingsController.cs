577b739709086294136e4519fbc0726d7023a881
kcwang <kcwang.work@gmail.com>
2020-09-29 17:24:53 +0800

    建立第一個 API：取得所有座位表

--- ten-minutes-aspnetcore/Controllers/SeatingsController.cs
+++ ten-minutes-aspnetcore/Controllers/SeatingsController.cs
@@ -7,9 +7,28 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ten_minutes_aspnetcore.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/Seatings")]
     [ApiController]
     public class SeatingsController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var seatings = new List<Seating>
+            {
+                new Seating { User = "Alice", Extension = "3333", CellPhone = "0911222333", Position = "1-1" },
+                new Seating { User = "Bob", Extension = "3111", CellPhone = "0922222333", Position = "1-2" }
+            };
+
+            return Ok(seatings);
+        }
+    }
+
+    public class Seating
+    {
+        public string User { get; set; }
+        public string Extension { get; set; }
+        public string CellPhone { get; set; }
+        public string Position { get; set; }
     }
 }
