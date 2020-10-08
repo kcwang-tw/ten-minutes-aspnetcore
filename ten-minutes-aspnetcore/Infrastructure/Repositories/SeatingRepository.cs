7d0a003df645d82917dbeaffbb6d925ff6fe89fd
kcwang <kcwang.work@gmail.com>
2020-10-08 11:50:45 +0800

    新增 SeatingRepository 並繼承介面

---
+++ ten-minutes-aspnetcore/Infrastructure/Repositories/SeatingRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ten_minutes_aspnetcore.Domain.Models;
+using ten_minutes_aspnetcore.Domain.Repositories;
+
+namespace ten_minutes_aspnetcore.Infrastructure.Repositories
+{
+    public class SeatingRepository : ISeatingRepository
+    {
+        public IEnumerable<Seating> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
